9a1bad76763200de9169124ff14d1adaf040acec
Amanda Williamson <aselliott4@aol.co.uk>
2019-09-04 21:14:19 +0100

    All tests passing, last method needs changed

---
+++ models/traveller.js
@@ -0,0 +1,40 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map(journey => journey.startLocation);
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map(journey => journey.endLocation);
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  const foundJourneys = this.journeys.filter( journey => {
+    return journey.transport === transport
+  });
+  return foundJourneys;
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  const journeys = this.journeys.filter( journey => {
+    return journey.distance >= minDistance
+  });
+  return journeys;
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((total, journey) => {
+    return total += journey.distance
+  }, 0);
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  let a = ['train', 'train', 'aeroplane', 'car', 'ferry'];
+  let unique = a.filter((item, i, ar) => ar.indexOf(item) === i);
+  return unique;
+};
+
+
+module.exports = Traveller;
